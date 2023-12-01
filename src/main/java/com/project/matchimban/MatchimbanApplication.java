bc13d393f228fd844417f3d3a71befc6434d5af5
2384320 <chdms1549@gmail.com>
2023-12-01 16:19:12 +0900

    chore: 초기 프로젝트 설정 및 구성

---
+++ src/main/java/com/project/matchimban/MatchimbanApplication.java
@@ -0,0 +1,13 @@
+package com.project.matchimban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchimbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchimbanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/matchimban/MatchimbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.matchimban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchimbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
