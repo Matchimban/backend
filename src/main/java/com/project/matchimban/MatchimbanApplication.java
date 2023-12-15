e89415b05d10f8559ad9d24b206e9af268c78026
2384320 <chdms1549@gmail.com>
2023-12-15 16:46:19 +0900

    feat: Swagger https 설정 추가

--- src/main/java/com/project/matchimban/MatchimbanApplication.java
+++ src/main/java/com/project/matchimban/MatchimbanApplication.java
@@ -1,9 +1,12 @@
 package com.project.matchimban;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://15.164.94.57:8080", description = "Default server URL")})
 @SpringBootApplication
 @EnableJpaAuditing
 public class MatchimbanApplication {
