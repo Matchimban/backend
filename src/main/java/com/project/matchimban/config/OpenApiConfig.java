b94a8a013ae2655de964cb0dd91ab1956f9ddf72
2384320 <chdms1549@gmail.com>
2023-12-15 17:01:52 +0900

    feat: Swagger Api 설정 추가

--- src/main/java/com/project/matchimban/config/OpenApiConfig.java
+++ src/main/java/com/project/matchimban/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.project.matchimban.config;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,7 @@ public class OpenApiConfig {
                 .version("v0.0.1")
                 .description("데모 프로젝트의 API 명세서입니다.");
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .components(new Components())
                 .info(info);
     }
