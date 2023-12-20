a94c6687455bbdfdedd72bbcf534e49cd29885b1
2384320 <chdms1549@gmail.com>
2023-12-20 21:26:34 +0900

    test: 테스트 코드 작성

---
+++ src/test/java/com/project/matchimban/UnitTest.java
@@ -0,0 +1,16 @@
+package com.project.matchimban;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UnitTest {
+    @Test
+    public void test1() {
+        // given
+        // when
+        // then
+        Assertions.assertThat(1).isEqualTo(2);
+    }
+}
