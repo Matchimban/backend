5b1620f0c4371a80a7a223129d190eaaf405de95
2384320 <chdms1549@gmail.com>
2023-12-20 21:45:27 +0900

    test: 빌드 테스트

--- src/main/java/com/project/matchimban/MatchimbanApplication.java
+++ src/main/java/com/project/matchimban/MatchimbanApplication.java
@@ -1,6 +1,6 @@
 package com.project.matchimban;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +12,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class MatchimbanApplication {
 
 	public static void main(String[] args) {
+
+
+
 		SpringApplication.run(MatchimbanApplication.class, args);
 	}
 
