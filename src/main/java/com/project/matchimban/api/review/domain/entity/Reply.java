15e649bd1e3fc2d5b36daa31b86324bd35b443a7
2384320 <chdms1549@gmail.com>
2024-01-12 22:12:09 +0900

    feat: reply 어노테이션 추가

--- src/main/java/com/project/matchimban/api/review/domain/entity/Reply.java
+++ src/main/java/com/project/matchimban/api/review/domain/entity/Reply.java
@@ -1,21 +1,24 @@
-package com.project.matchimban.api.review.domain;
+package com.project.matchimban.api.review.domain.entity;
 
+import com.project.matchimban.api.review.domain.enums.ReplyStatus;
 import com.project.matchimban.common.global.TimeEntity;
 import com.project.matchimban.api.user.domain.entity.User;
 import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
 
 @Entity
 @Getter
-@Table(name = "reply")
 public class Reply extends TimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,7 +32,11 @@ public class Reply extends TimeEntity {
     @JoinColumn(name = "review_id")
     private Review review;
 
+    @Column(nullable = false)
     private String content;
 
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    @ColumnDefault("'PUBLISHED'")
     private ReplyStatus status;
 }
