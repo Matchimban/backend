be3a20bc21a6a98b9c02b9b33cc414b49aea000a
woonie155 <woonie155@gmail.com>
2024-01-14 02:30:24 +0900

    feat: ReservationMenu 도메인 추가
    ReservationMenu은 예약 시점의 예약한 메뉴 정보를 저장하기 위함

---
+++ src/main/java/com/project/matchimban/api/reservation/domain/entity/ReservationMenu.java
@@ -0,0 +1,29 @@
+package com.project.matchimban.api.reservation.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "reservation_menu")
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ReservationMenu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_reservation_id", nullable = false)
+    private RestaurantReservation restaurantReservation;
+
+
+    private String menuName; //메뉴 이름
+
+    private Integer regularPrice;//정가
+
+    private Integer paymentAmount;//결제가
+}
