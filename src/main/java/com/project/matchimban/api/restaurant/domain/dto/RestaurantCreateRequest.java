12068f66c26825bb2bb4340a9e022df65f34eb7a
2384320 <chdms1549@gmail.com>
2024-01-15 22:18:22 +0900

    feat: 매장 등록 DTO 생성

---
+++ src/main/java/com/project/matchimban/api/restaurant/domain/dto/RestaurantCreateRequest.java
@@ -0,0 +1,49 @@
+package com.project.matchimban.api.restaurant.domain.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.NotBlank;
+
+@Schema(description = "매장 등록 DTO")
+public class RestaurantCreateRequest {
+
+    @Schema(description = "카테고리")
+    @NotBlank(message = "카테고리를 선택해주세요.")
+    private String category;
+
+    @Schema(description = "상호명")
+    @NotBlank(message = "상호명을 입력해주세요.")
+    private String name;
+
+    @Schema(description = "사업자 등록번호")
+    @NotBlank(message = "사업자 등록번호를 입력해주세요.")
+    private String businessNumber;
+
+    @Schema(description = "원산지")
+    @NotBlank(message = "원산지를 입력해주세요.")
+    private String originCountry;
+
+    @Schema(description = "주소-시/도")
+    @NotBlank(message = "주소-시/도를 입력해주세요.")
+    private String addrSido;
+
+    @Schema(description = "주소-시/군/구")
+    @NotBlank(message = "주소-시/군/구를 입력해주세요.")
+    private String addrSigg;
+
+    @Schema(description = "주소-읍/면/동")
+    @NotBlank(message = "주소-읍/면/동을 입력해주세요.")
+    private String addrEmd;
+
+    @Schema(description = "상세 주소")
+    @NotBlank(message = "상세 주소를 입력해주세요.")
+    private String addrDetail;
+
+    @Schema(description = "위도")
+    @NotBlank(message = "위도를 입력해주세요.")
+    private double latitude;
+
+    @Schema(description = "경도")
+    @NotBlank(message = "경도를 입력해주세요.")
+    private double longitude;
+}
