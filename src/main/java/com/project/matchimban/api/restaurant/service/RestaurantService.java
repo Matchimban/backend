9e27fab0241353cee5e4e68e2ad285334d2d8f8c
2384320 <chdms1549@gmail.com>
2024-01-30 22:01:45 +0900

    feat: 이미지 등록 관련 메서드 추가

--- src/main/java/com/project/matchimban/api/restaurant/service/RestaurantService.java
+++ src/main/java/com/project/matchimban/api/restaurant/service/RestaurantService.java
@@ -2,8 +2,11 @@ package com.project.matchimban.api.restaurant.service;
 
 import com.project.matchimban.api.auth.security.model.CustomUserDetails;
 import com.project.matchimban.api.restaurant.domain.dto.RestaurantCreateRequest;
-import com.project.matchimban.api.restaurant.domain.entity.Restaurant;
+import com.project.matchimban.api.restaurant.domain.dto.RestaurantImageCreateRequest;
+
+import java.util.List;
 
 public interface RestaurantService {
-    Restaurant createRestaurant(RestaurantCreateRequest dto, CustomUserDetails userDetails);
+    void createRestaurant(RestaurantCreateRequest dto, CustomUserDetails userDetails);
+    void registerRestaurantImage(Long restaurantId, List<RestaurantImageCreateRequest> images);
 }
--- src/main/java/com/project/matchimban/api/restaurant/service/impl/RestaurantServiceImpl.java
+++ src/main/java/com/project/matchimban/api/restaurant/service/impl/RestaurantServiceImpl.java
@@ -2,7 +2,9 @@ package com.project.matchimban.api.restaurant.service.impl;
 
 import com.project.matchimban.api.auth.security.model.CustomUserDetails;
 import com.project.matchimban.api.restaurant.domain.dto.RestaurantCreateRequest;
+import com.project.matchimban.api.restaurant.domain.dto.RestaurantImageCreateRequest;
 import com.project.matchimban.api.restaurant.domain.entity.Restaurant;
+import com.project.matchimban.api.restaurant.domain.entity.RestaurantImage;
 import com.project.matchimban.api.restaurant.repository.RestaurantRepository;
 import com.project.matchimban.api.restaurant.service.RestaurantService;
 import com.project.matchimban.api.user.domain.entity.User;
@@ -14,6 +16,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -21,7 +25,7 @@ public class RestaurantServiceImpl implements RestaurantService {
     private final RestaurantRepository restaurantRepository;
     private final UserRepository userRepository;
 
-    public Restaurant createRestaurant(RestaurantCreateRequest dto, CustomUserDetails userDetails) {
+    public void createRestaurant(RestaurantCreateRequest dto, CustomUserDetails userDetails) {
         User user = userRepository.findByEmail(userDetails.getUsername())
                 .orElseThrow(() -> new SVCException(ErrorConstant.NOT_FOUND_USER));
 
@@ -34,7 +38,17 @@ public class RestaurantServiceImpl implements RestaurantService {
                 dto.getLongitude()
         );
 
-        Restaurant restaurant = Restaurant.createRestaurant(dto, user, address);
-        return restaurantRepository.save(restaurant);
+        Restaurant.createRestaurant(dto, user, address);
+    }
+
+    public void registerRestaurantImage(Long id, List<RestaurantImageCreateRequest> images) {
+        Restaurant restaurant = restaurantRepository.findById(id)
+                .orElseThrow(() -> new SVCException(ErrorConstant.RESTAURANT_ERROR_NOT_FOUND_RESTAURANT));
+
+//        // 저장
+//        for (RestaurantImageCreateRequest request : images) {
+//            RestaurantImage image = RestaurantImage.createRestaurantImage(restaurant, request);
+//
+//        }
     }
 }
