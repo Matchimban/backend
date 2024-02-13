d18b7f703e823c34eeb25ac4fc50108eeb9fa48d
roovies <roovies77@gmail.com>
2024-02-13 17:47:05 +0900

    refactor: Swagger 인가 설정 및 설정

--- src/main/java/com/project/matchimban/api/auth/jwt/domain/dto/TokenDTO.java
+++ src/main/java/com/project/matchimban/api/auth/jwt/domain/dto/TokenDTO.java
@@ -1,12 +1,18 @@
 package com.project.matchimban.api.auth.jwt.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
+@Schema(description = "Token 응답 DTO")
 public class TokenDTO {
+
+    @Schema(description = "Access Token")
     private String accessToken;
+
+    @Schema(description = "Refresh Token")
     private String refreshToken;
 }
