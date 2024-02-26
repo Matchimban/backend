c4961ba89d5838c30ccde7795122821d252bfe9e
woonie155 <woonie155@gmail.com>
2024-02-26 16:14:11 +0900

    chore: redis 로컬 환경 분리

--- src/main/java/com/project/matchimban/common/config/LocalRedisConfig.java
+++ src/main/java/com/project/matchimban/common/config/LocalRedisConfig.java
@@ -1,5 +1,6 @@
 package com.project.matchimban.common.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -11,32 +12,53 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.embedded.RedisServer;
 
-@Profile("server")
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Slf4j
+@Profile({"local", "test"})
 @Configuration
-public class RedisConfig {
-
+public class LocalRedisConfig {
+    @Value("${spring.redis.port}")
+    private int redisPort;
+    private RedisServer redisServer;
     @Value("${spring.redis.host}")
-    private String host;
-
+    private String redisHost;
     @Value("${spring.redis.port}")
     private int port;
-
-    @Value("${spring.redis.password}")
-    private String password;
     private static final String REDISSON_HOST_PREFIX = "redis://";
+    @PostConstruct
+    public void startRedis() {
+        redisServer = RedisServer.builder()
+                .port(redisPort)
+                .setting("maxmemory 128M")
+                .build();
+        try {
+            redisServer.start();
+        } catch (Exception e){
+            log.error("{}", e);
+        }
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
-        redisConfiguration.setHostName(host);
+        redisConfiguration.setHostName(redisHost);
         redisConfiguration.setPort(port);
-        redisConfiguration.setPassword(password);
         LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisConfiguration);
         return lettuceConnectionFactory;
     }
-
     @Bean
     public RedisTemplate<?, ?> redisTemplate() {
         // RedisTemplate을 이용하여 Redis에 CRUD 작업 수행
@@ -59,8 +81,7 @@ public class RedisConfig {
         RedissonClient redisson = null;
         Config config = new Config();
         config.useSingleServer()
-                .setAddress(REDISSON_HOST_PREFIX + host + ":" + port)
-                .setPassword(password);
+                .setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
         redisson = Redisson.create(config);
         return redisson;
     }
