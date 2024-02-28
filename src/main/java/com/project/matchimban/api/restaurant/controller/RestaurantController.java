2903f003a98ba441c53da8c3d7b5268d2dad0cd5
2384320 <chdms1549@gmail.com>
2024-02-28 21:58:11 +0900

    feat: 스웨거 어노테이션 추가

--- src/main/java/com/project/matchimban/api/restaurant/controller/RestaurantController.java
+++ src/main/java/com/project/matchimban/api/restaurant/controller/RestaurantController.java
@@ -1,44 +1,60 @@
 package com.project.matchimban.api.restaurant.controller;
 
 import com.project.matchimban.api.auth.security.model.CustomUserDetails;
-import com.project.matchimban.api.restaurant.domain.dto.RestaurantCreateRequest;
-import com.project.matchimban.api.restaurant.domain.dto.RestaurantImageCreateRequest;
+import com.project.matchimban.api.restaurant.domain.dto.RestaurantRegisterRequest;
 import com.project.matchimban.api.restaurant.service.RestaurantService;
 import com.project.matchimban.common.response.ResultData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
+@Tag(name = "Restaurant", description = "매장 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/restaurant")
 public class RestaurantController {
     private final RestaurantService restaurantService;
 
+    @Operation(summary = "매장 등록", description = "매장, 매장 이미지, 메뉴를 등록합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "20000", description = "등록 성공"),
+            @ApiResponse(responseCode = "40001", description = "입력값 유효성 검증 실패"),
+            @ApiResponse(responseCode = "60000", description = "이미 존재하는 회원")
+    })
     @PostMapping("")
-    public ResponseEntity<Object> createRestaurant(
-            @RequestBody @Valid RestaurantCreateRequest dto,
+    public ResponseEntity<Object> registerRestaurant(
+            @ModelAttribute @Valid RestaurantRegisterRequest request,
             @AuthenticationPrincipal CustomUserDetails userDetails
     ) {
-        restaurantService.createRestaurant(dto, userDetails);
+        restaurantService.registerRestaurant(request, userDetails);
         return new ResponseEntity<>(new ResultData(), HttpStatus.CREATED);
     }
 
-    @PostMapping("/{restaurantId}/image")
-    public ResponseEntity<Object> registerRestaurantImage(
-            @PathVariable Long restaurantId,
-            @RequestBody @Valid List<RestaurantImageCreateRequest> images) {
-        restaurantService.registerRestaurantImage(restaurantId, images);
-        return new ResponseEntity<>(new ResultData(), HttpStatus.CREATED);
-    }
+//    @PostMapping("")
+//    public ResponseEntity<Object> registerRestaurant(
+//            @RequestBody @Valid RestaurantCreateRequest dto,
+//            @AuthenticationPrincipal CustomUserDetails userDetails
+//    ) {
+//        restaurantService.registerRestaurant(dto, userDetails);
+//        return new ResponseEntity<>(new ResultData(), HttpStatus.CREATED);
+//    }
+
+//    @PostMapping("/{restaurantId}/image")
+//    public ResponseEntity<Object> registerRestaurantImage(
+//            @PathVariable Long restaurantId,
+//            @RequestBody @Valid List<RestaurantImageCreateRequest> request) {
+//        restaurantService.registerRestaurantImage(restaurantId, images);
+//        return new ResponseEntity<>(new ResultData(), HttpStatus.CREATED);
+//    }
 }
