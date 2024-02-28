400813f0e00e8b8c1e3e3cca5f2402cda4ee6a54
2384320 <chdms1549@gmail.com>
2024-02-28 21:59:43 +0900

    refactor: registerRestaurant 수정

--- src/main/java/com/project/matchimban/api/restaurant/service/RestaurantService.java
+++ src/main/java/com/project/matchimban/api/restaurant/service/RestaurantService.java
@@ -1,12 +1,12 @@
 package com.project.matchimban.api.restaurant.service;
 
 import com.project.matchimban.api.auth.security.model.CustomUserDetails;
-import com.project.matchimban.api.restaurant.domain.dto.RestaurantCreateRequest;
 import com.project.matchimban.api.restaurant.domain.dto.RestaurantImageCreateRequest;
+import com.project.matchimban.api.restaurant.domain.dto.RestaurantRegisterRequest;
 
 import java.util.List;
 
 public interface RestaurantService {
-    void createRestaurant(RestaurantCreateRequest dto, CustomUserDetails userDetails);
+    void registerRestaurant(RestaurantRegisterRequest request, CustomUserDetails userDetails);
     void registerRestaurantImage(Long restaurantId, List<RestaurantImageCreateRequest> images);
 }
