7a105425e10e458753b1dd5f90415cd7e83ac651
2384320 <chdms1549@gmail.com>
2024-03-02 23:33:04 +0900

    refactor: inputStream 에러 수집 추가

--- src/main/java/com/project/matchimban/common/modules/S3Service.java
+++ src/main/java/com/project/matchimban/common/modules/S3Service.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.project.matchimban.common.exception.ErrorConstant;
 import com.project.matchimban.common.exception.SVCException;
+import com.project.matchimban.common.global.FileInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -27,11 +29,11 @@ public class S3Service {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    public String saveFile(MultipartFile file) {
-        if (file == null)
-            throw new SVCException(ErrorConstant.FILE_ERROR_NULL_FILE);
-
+    public Optional<FileInfo> saveFile(MultipartFile file) {
         String originalFileName = file.getOriginalFilename();
+
+        // originalFileName이 null이 나올 수 있음. -> 파일이 없다는 뜻
+
         String extension = StringUtils.getFilenameExtension(originalFileName);
         String today = new SimpleDateFormat("yyMMdd").format(new Date());
         String uuid = UUID.randomUUID().toString();
@@ -47,9 +49,18 @@ public class S3Service {
         metadata.setContentType(file.getContentType());
         metadata.setContentLength(file.getSize());
 
-        amazonS3Client.putObject(bucket, today + savedFileName, file.getInputStream(), metadata);
+        try {
+            amazonS3Client.putObject(bucket, today + savedFileName, file.getInputStream(), metadata);
+        } catch (IOException e) {
+            throw new SVCException(ErrorConstant.FILE_ERROR_NULL_FILE);
+        }
 
-        return savedFileName;
+        return Optional.ofNullable(
+                FileInfo.builder()
+                        .originalFileName(originalFileName)
+                        .savedFileName(savedFileName)
+                        .build()
+        );
     }
 
 //    public Optional<FileInfo> uploadFile(MultipartFile file) {
