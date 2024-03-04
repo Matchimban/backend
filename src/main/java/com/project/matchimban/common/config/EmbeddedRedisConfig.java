429a4cfd567aba93efedeca145f44329e5b332eb
woonie155 <woonie155@gmail.com>
2024-03-04 17:13:10 +0900

    chore: redis 환경을 도커환경과 임베디드환경으로 분리

--- src/main/java/com/project/matchimban/common/config/EmbeddedRedisConfig.java
+++ src/main/java/com/project/matchimban/common/config/EmbeddedRedisConfig.java
@@ -12,7 +12,6 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import redis.embedded.RedisServer;
 
@@ -20,9 +19,9 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 @Slf4j
-@Profile({"local", "test"})
+@Profile({"embedded", "test"})
 @Configuration
-public class LocalRedisConfig {
+public class EmbeddedRedisConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
     private RedisServer redisServer;
