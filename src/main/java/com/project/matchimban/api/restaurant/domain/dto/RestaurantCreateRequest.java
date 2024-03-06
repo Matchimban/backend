c00ef06443ec82bc2ebdea7184528de696da6302
2384320 <chdms1549@gmail.com>
2024-03-06 22:18:20 +0900

    refactor: @Setter 추가

--- src/main/java/com/project/matchimban/api/restaurant/domain/dto/RestaurantCreateRequest.java
+++ src/main/java/com/project/matchimban/api/restaurant/domain/dto/RestaurantCreateRequest.java
@@ -3,11 +3,13 @@ package com.project.matchimban.api.restaurant.domain.dto;
 import com.project.matchimban.api.restaurant.domain.enums.RestaurantCategory;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
+@Setter
 @Schema(description = "매장 등록 DTO")
 public class RestaurantCreateRequest {
     @Schema(description = "카테고리")
