984f0da67237614102fa6bc9a3ed571e7bb27205
2384320 <chdms1549@gmail.com>
2024-03-06 23:33:17 +0900

    refactor: menu 위치 변경

--- src/main/java/com/project/matchimban/api/menu/repository/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.matchimban.api.menu.repository;
-
-import com.project.matchimban.api.menu.domain.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<Menu, Long> {
-}
