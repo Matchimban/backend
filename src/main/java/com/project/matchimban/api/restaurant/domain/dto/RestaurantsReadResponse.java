9ec06a9fee4d703d06917c360524849206362147
2384320 <chdms1549@gmail.com>
2024-03-07 21:12:15 +0900

    feat: 매장 전체 조회 response 생성

---
+++ src/main/java/com/project/matchimban/api/restaurant/domain/dto/RestaurantsReadResponse.java
@@ -0,0 +1,43 @@
+package com.project.matchimban.api.restaurant.domain.dto;
+
+import com.project.matchimban.api.restaurant.domain.entity.Restaurant;
+import com.project.matchimban.api.restaurant.domain.enums.RestaurantCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "매장 전체 조회 DTO")
+public class RestaurantsReadResponse {
+
+    @Schema(description = "매장 id")
+    private Long id;
+
+    @Schema(description = "메장 카테고리")
+    private RestaurantCategory category;
+
+    @Schema(description = "메장 이름")
+    private String name;
+
+    @Schema(description = "매장 주소(시/도)")
+    private String addrSido;
+
+    @Schema(description = "매장 MAIN 사진")
+    private String imageUrl;
+
+    public static RestaurantsReadResponse createRestaurantsReadResponse(Restaurant restaurant, String imageUrl) {
+        return RestaurantsReadResponse.builder()
+                .id(restaurant.getId())
+                .category(restaurant.getCategory())
+                .name(restaurant.getName())
+                .addrSido(restaurant.getAddress().getAddrSido())
+                .imageUrl(imageUrl)
+                .build();
+    }
+}
