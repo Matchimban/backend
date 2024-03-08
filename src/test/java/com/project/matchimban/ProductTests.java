4fa38da8c743ea69cbd24446d2569fd5fe0289c9
2384320 <chdms1549@gmail.com>
2024-03-08 22:44:05 +0900

    test: redis 테스트 코드 작성

---
+++ src/test/java/com/project/matchimban/ProductTests.java
@@ -0,0 +1,67 @@
+package com.project.matchimban;
+
+import com.project.matchimban.common.test.Product;
+import com.project.matchimban.common.test.ProductRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class ProductTests {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    private Product product;
+
+    @BeforeEach
+    void setUp() {
+        product = new Product(1L, "테스트_상품", 20000);
+        productRepository.save(product);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        productRepository.deleteAll();
+    }
+
+    @Test
+    public void saveRedisTest() {
+        Product persistProduct = productRepository.findById(product.getId())
+                .orElseThrow(RuntimeException::new);
+
+        assertThat(persistProduct.getId()).isEqualTo(product.getId());
+        assertThat(persistProduct.getName()).isEqualTo(product.getName());
+        assertThat(persistProduct.getPrice()).isEqualTo(product.getPrice());
+    }
+
+    @Test
+    public void redis_update_test() {
+        Product persistProduct = productRepository.findById(product.getId())
+                .orElseThrow(RuntimeException::new);
+
+        // when
+        persistProduct.changePrice(35000);
+        productRepository.save(persistProduct);
+
+        // then
+        assertThat(persistProduct.getPrice()).isEqualTo(35000);
+    }
+
+    @Test
+    public void redis_delete_test() {
+        // when
+        productRepository.delete(product);
+        Optional<Product> deletedProduct = productRepository.findById(product.getId());
+
+        // then
+        assertTrue(deletedProduct.isEmpty());
+    }
+}
