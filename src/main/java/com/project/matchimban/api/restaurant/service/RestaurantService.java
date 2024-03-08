97d674f6dbf0d1a3ba824a95d8ebd35424654c2b
2384320 <chdms1549@gmail.com>
2024-03-08 16:07:57 +0900

    feat: 매장 전체 조회 기능 메서드 추가

--- src/main/java/com/project/matchimban/api/restaurant/service/RestaurantService.java
+++ src/main/java/com/project/matchimban/api/restaurant/service/RestaurantService.java
@@ -2,7 +2,11 @@ package com.project.matchimban.api.restaurant.service;
 
 import com.project.matchimban.api.auth.security.model.CustomUserDetails;
 import com.project.matchimban.api.restaurant.domain.dto.RestaurantRegisterRequest;
+import com.project.matchimban.api.restaurant.domain.dto.RestaurantsReadResponse;
+
+import java.util.List;
 
 public interface RestaurantService {
     void registerRestaurant(RestaurantRegisterRequest request, CustomUserDetails userDetails);
+    List<RestaurantsReadResponse> getRestaurants();
 }
