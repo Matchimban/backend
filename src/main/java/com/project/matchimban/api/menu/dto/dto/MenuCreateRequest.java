20ef9b9f74552da99e66d0561a94e3aa797a3f72
2384320 <chdms1549@gmail.com>
2024-03-14 00:45:34 +0900

    feat: MenuCreateRequest 추가

---
+++ src/main/java/com/project/matchimban/api/menu/dto/dto/MenuCreateRequest.java
@@ -0,0 +1,25 @@
+package com.project.matchimban.api.menu.dto.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Schema(description = "메뉴 등록 DTO")
+public class MenuCreateRequest {
+
+    @Schema(description = "메뉴 이름")
+    @NotBlank(message = "메뉴 이름을 등록해주세요")
+    private String name;
+
+    @Schema(description = "메뉴 가격")
+    @NotNull(message = "메뉴 가격을 등록해주세요")
+    private int price;
+
+    @Schema(description = "메뉴 이미지")
+    @NotNull(message = "메뉴 이미지를 등록해주세요")
+    private MultipartFile image;
+}
