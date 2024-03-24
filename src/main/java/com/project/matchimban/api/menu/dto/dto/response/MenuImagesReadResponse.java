72d32ebcb1c16f0e3832288b5d26383c725584ea
2384320 <chdms1549@gmail.com>
2024-03-24 22:07:14 +0900

    feat: 메뉴 전체 조회 이미지 정보 DTO 추가

---
+++ src/main/java/com/project/matchimban/api/menu/dto/dto/response/MenuImagesReadResponse.java
@@ -0,0 +1,20 @@
+package com.project.matchimban.api.menu.dto.dto.response;
+
+import com.querydsl.core.annotations.QueryProjection;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Schema(description = "메뉴 조회 이미지 정보 DTO")
+@EqualsAndHashCode(of = "savedFileUrl")
+public class MenuImagesReadResponse {
+    private Long imageId;
+    private String savedFileUrl;
+
+    @QueryProjection
+    public MenuImagesReadResponse(Long imageId, String savedFileUrl) {
+        this.imageId = imageId;
+        this.savedFileUrl = savedFileUrl;
+    }
+}
